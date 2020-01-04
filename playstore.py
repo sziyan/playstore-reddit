dfc3134561ffe1ed4c670455ebff59899da910ef
sziyan <sziyan@hotmai.com>
2020-01-04 16:35:35 +0800

    Updated error catching.

--- playstore.py
+++ playstore.py
@@ -42,65 +42,73 @@ def get_all_app_requests(linkme_requests):
         for item in rq.split(","):
             apps_list.append(item)
     return apps_list
-try:
-    for comments in subreddit.stream.comments(skip_existing=True):
-        if comments.author.name == Config.username:
-            continue
-        else:
+
+for comments in subreddit.stream.comments(skip_existing=True):
+    if comments.author.name == Config.username:
+        continue
+    else:
+        try:
             message = ""
             body = comments.body.lower()
-            clean_text = get_clean_comment(body)
-            link_me_requests = link_me_regex.findall(clean_text)
-            app_count = get_no_apps(link_me_requests)
-            app_list = get_all_app_requests(link_me_requests)
-            count = 1
-            if app_count > 0:
-                logging.info("{} is searching for {} app(s): {}".format(comments.author.name, app_count, ",".join(app_list)))
-                print("{} is searching for {} app(s): {}".format(comments.author.name, app_count, ",".join(app_list)))
-                if app_count > Config.max_apps:
-                    msg = "You have searched for more then {} apps. I will only link to the first {} apps.\n\n".format(Config.max_apps, Config.max_apps)
-                    message +=msg
-                for search in app_list:
-                    if count > Config.max_apps:
-                        continue
-                    result = play.search(search,page=1, detailed=True)
-                    if not result:
-                        continue
-                    result = result[0]
-                    title = result.get('title')
-                    score = result.get('score')
-                    url = result.get('url')
-                    search_manual = 'https://play.google.com/store/search?q={}'.format(search)
-                    if result.get('free') is True:
-                        price = 'Free'
+            if 'linkme' in body:
+                clean_text = get_clean_comment(body)
+                link_me_requests = link_me_regex.findall(clean_text)
+                app_count = get_no_apps(link_me_requests)
+                app_list = get_all_app_requests(link_me_requests)
+                count = 1
+                if app_count > 0:
+                    logging.info("{} is searching for {} app(s): {}".format(comments.author.name, app_count, ",".join(app_list)))
+                    print("{} is searching for {} app(s): {}".format(comments.author.name, app_count, ",".join(app_list)))
+                    if app_count > Config.max_apps:
+                        msg = "You have searched for more then {} apps. I will only link to the first {} apps.\n\n".format(Config.max_apps, Config.max_apps)
+                        message +=msg
+                    for search in app_list:
+                        if count > Config.max_apps:
+                            continue
+                        result = play.search(search,page=1, detailed=True)
+                        if not result:
+                            continue
+                        result = result[0]
+                        title = result.get('title')
+                        score = result.get('score')
+                        url = result.get('url')
+                        search_manual = 'https://play.google.com/store/search?q={}'.format(search)
+                        if result.get('free') is True:
+                            price = 'Free'
+                        else:
+                            price = result.get('price')
+                        if result.get('iap') is True:
+                            price+= ' with IAP'
+                        if app_count == 1:
+                            developer = result.get('developer')
+                            installs = result.get('installs')
+                            description = result.get('description')
+                            description = description.split(" ")
+                            desc_output = " ".join(description[0:26]) + " ..."
+                            msg = "**[{}]({})** by {} | {} | {} installs \n\n> {}".format(title,url,developer,price,installs,desc_output)
+                        else:
+                            msg = "[{}]({}) - {} - [Search manually]({}) \n\n".format(title,url, price, search_manual)
+                        count+=1
+                        message+=msg
+                    if message != "":
+                        message+="\n\n\n\n --- \n\n\n\n \n\nI am a new bot. Please help me by private messaging me any bugs you see. Thank You. \n\nGoogle Play links bot by /u/lonerzboy"
+                        comments.reply(message)
+                        logging.info('{} completed app search successfully.'.format(comments.author.name))
+                        print('{} completed app search successfully.'.format(comments.author.name))
                     else:
-                        price = result.get('price')
-                    if result.get('iap') is True:
-                        price+= ' with IAP'
-                    if app_count == 1:
-                        developer = result.get('developer')
-                        installs = result.get('installs')
-                        description = result.get('description')
-                        description = description.split(" ")
-                        desc_output = " ".join(description[0:26]) + " ..."
-                        msg = "**[{}]({})** by {} | {} | {} installs \n\n> {}".format(title,url,developer,price,installs,desc_output)
-                    else:
-                        msg = "[{}]({}) - {} - [Search manually]({}) \n\n".format(title,url, price, search_manual)
-                    count+=1
-                    message+=msg
-                if message != "":
-                    message+="\n\n\n\n --- \n\n\n\n \n\nI am a new bot. Please help me by private messaging me any bugs you see. Thank You. \n\nGoogle Play links bot by /u/lonerzboy"
-                    comments.reply(message)
-                    logging.info('{} completed app search successfully.'.format(comments.author.name))
-                    print('{} completed app search successfully.'.format(comments.author.name))
-                else:
-                    logging.info("{} searched for an empty game.".format(comments.author.name))
-                    continue
+                        logging.info("{} searched for an empty game.".format(comments.author.name))
+                        continue
             else:
-                logging.info("{} searched for an empty game.".format(comments.author.name))
                 continue
+        except praw.exceptions.APIException as api_exception:
+            logging.error(
+                "Error: {} - {} - {}".format(api_exception.error_type, api_exception.field, api_exception.message))
+            print(
+                "API Error: {} - {} - {}".format(api_exception.error_type, api_exception.field, api_exception.message))
+            pass
+        except praw.exceptions.ClientException:
+            logging.error("Client Error: Client exception")
+            pass
 
-except praw.exceptions.APIException as api_exception:
-    logging.error("Error: {} - {} - {}".format(api_exception.error_type, api_exception.field, api_exception.message))
-    print("Error: {} - {} - {}".format(api_exception.error_type, api_exception.field, api_exception.message))
+
 
